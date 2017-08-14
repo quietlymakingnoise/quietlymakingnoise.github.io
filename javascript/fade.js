538536e4c58720ca7df11964c24cc6707e64bbd5
quietlymakingnoise <30625376+quietlymakingnoise@users.noreply.github.com>
2017-08-14 11:08:07 +0100

    All Change

--- javascript/fade.js
+++ javascript/fade.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $('#fade').fadeIn(200);
+    $('#fade').fadeIn(400);
 });
+
+$('#releaseImg').on('hover', function() {
+  opacity: 0.5;
+});
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
