a8333960d9673dfb4ac20944f76d618ba1e55046
quietlymakingnoise <30625376+quietlymakingnoise@users.noreply.github.com>
2017-09-03 11:19:54 +0100

    Finally bloody done
    done

--- javascript/fade.js
+++ javascript/fade.js
@@ -17,7 +17,7 @@ document.addEventListener('play', function(e) {
 
 var feed = new Instafeed({
     get: 'user',
-    userId: '5882151769',
+    userId: '4039901141',
     limit:'30',
     resolution: 'standard_resolution',
     target: 'instafeed',
@@ -25,3 +25,14 @@ var feed = new Instafeed({
     clientId: '	ba6ff2c31594481286faf391dfa76eba'
 });
 feed.run();
+
+var feed2 = new Instafeed({
+    get: 'user',
+    userId: '5881343163',
+    limit:'30',
+    resolution: 'standard_resolution',
+    target: 'instafeed2',
+    accessToken: '5882151769.ba6ff2c.b9c1c4afc5c44086bf93ed026854c1d0',
+    clientId: '	ba6ff2c31594481286faf391dfa76eba'
+});
+feed2.run();
