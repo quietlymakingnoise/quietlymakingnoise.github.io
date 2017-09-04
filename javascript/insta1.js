01689d02fd3bc0326eccdd6617b6d92cae4de398
quietlymakingnoise <30625376+quietlymakingnoise@users.noreply.github.com>
2017-09-04 21:19:15 +0100

    g

---
+++ javascript/insta1.js
@@ -0,0 +1,10 @@
+var feed = new Instafeed({
+    get: 'user',
+    userId: '4039901141',
+    limit:'6',
+    resolution: 'standard_resolution',
+    target: 'instafeed',
+    accessToken: '5882151769.ba6ff2c.b9c1c4afc5c44086bf93ed026854c1d0',
+    clientId: '	ba6ff2c31594481286faf391dfa76eba'
+});
+feed.run();
---
+++ javascript/insta2.js
@@ -0,0 +1,10 @@
+var feed2 = new Instafeed({
+    get: 'user',
+    userId: '5881343163',
+    limit:'3',
+    resolution: 'standard_resolution',
+    target: 'instafeed2',
+    accessToken: '5882151769.ba6ff2c.b9c1c4afc5c44086bf93ed026854c1d0',
+    clientId: '	ba6ff2c31594481286faf391dfa76eba'
+});
+feed2.run();
