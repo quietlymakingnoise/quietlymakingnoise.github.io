f083d5666a53474c49775c34e3cc635870e15546
quietlymakingnoise <30625376+quietlymakingnoise@users.noreply.github.com>
2017-09-04 21:31:45 +0100

    er

--- javascript/insta1.js
+++ javascript/insta1.js
@@ -3,7 +3,7 @@ var feed = new Instafeed({
     userId: '4039901141',
     limit:'6',
     resolution: 'standard_resolution',
-    target: 'instafeed2',
+    target: 'instafeed',
     accessToken: '5882151769.ba6ff2c.b9c1c4afc5c44086bf93ed026854c1d0',
     clientId: '	ba6ff2c31594481286faf391dfa76eba'
 });
--- javascript/insta2.js
+++ javascript/insta2.js
@@ -3,7 +3,7 @@ var feed2 = new Instafeed({
     userId: '5881343163',
     limit:'3',
     resolution: 'standard_resolution',
-    target: 'instafeed',
+    target: 'instafeed2',
     accessToken: '5882151769.ba6ff2c.b9c1c4afc5c44086bf93ed026854c1d0',
     clientId: '	ba6ff2c31594481286faf391dfa76eba'
 });
